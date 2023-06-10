1f5cd46c7e7e3c36403bde43126496a5ba3366b3
o7q <jev.o7q@gmail.com>
2023-06-09 22:04:06 -0500

    Update download links

--- assets/pages/index/files/scripts.js
+++ assets/pages/index/files/scripts.js
@@ -1,6 +1,6 @@
 function bodyInit()
 {
-    var ver = "v1.0.0.0";
+    var ver = "v1.0.1.0";
 
     document.getElementById("downloadID").href = "https://github.com/o7q/MediaConverter/releases/download/" + ver + "/MediaConverter." + ver + ".zip";
     document.getElementById("downloadID").innerHTML = "Download " + ver;
