b347ff2b29af04cd527f0e6383b8d4749e1139fe
o7q <jev.o7q@gmail.com>
2024-03-28 19:54:52 -0500

    Update scripts.js

--- assets/pages/index/files/scripts.js
+++ assets/pages/index/files/scripts.js
@@ -1,6 +1,6 @@
 function bodyInit()
 {
-    var VERSION = "v1.1.0.0";
+    var VERSION = "v1.1.1.0";
 
     document.getElementById("downloadID").href = "https://github.com/o7q/MediaConverter/releases/download/" + VERSION + "/MediaConverter." + VERSION + ".zip";
     document.getElementById("downloadID").innerHTML = "Download " + VERSION;
